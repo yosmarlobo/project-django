20ca5e5412597edf3d37cb4974a8b9236016dca7
yosmarlobo <94659644+yosmarlobo@users.noreply.github.com>
2022-08-28 00:51:31 -0500

    my_portfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://secure-earth-66335.herokuapp.com/',
+ALLOWED_HOSTS = ['secure-earth-66335.herokuapp.com/',
                  'localhost', '127.0.0.1']
 
 
